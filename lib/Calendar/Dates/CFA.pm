7eac86a323ac65f1e375b211c52bac60c540f779
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-14 21:44:05 +0700

    Move data to __DATA__

--- lib/Calendar/Dates/CFA.pm
+++ lib/Calendar/Dates/CFA.pm
@@ -11,33 +11,6 @@ use Role::Tiny::With;
 
 with 'Calendar::DatesRoles::FromData';
 
-our @ENTRIES = (
-    # dec2018exam
-    {date=>'2019-01-23', summary=>'Exam results announcement (Dec 2018, Levels I & II)', tags=>['dec2018exam']},
-
-    # jun2019exam
-    {date=>'2018-10-15', summary=>'First deadline to request disability accommodations', tags=>['jun2019exam']},
-    {date=>'2018-10-17', summary=>'Early registration fee deadline', tags=>['jun2019exam']},
-    {date=>'2019-02-13', summary=>'Standard registration fee deadline', tags=>['jun2019exam']},
-    {date=>'2019-02-18', summary=>'Second deadline to request disability accommodations', tags=>['jun2019exam']},
-    {date=>'2019-03-13', summary=>'Final (late) registration fee deadline', tags=>['jun2019exam']},
-    {date=>'2019-03-18', summary=>'Final deadline to request disability accommodations', tags=>['jun2019exam']},
-    # mid-may 2019, admission tickets available
-    {date=>'2019-06-11', summary=>'Deadline for submission of test center change requests', tags=>['jun2019exam']},
-    {date=>'2019-06-15', summary=>'Exam day: Asia-Pacific (Levels II & III); Americas and EMEA (all levels)', tags=>['jun2019exam']},
-    {date=>'2019-06-16', summary=>'Exam day: Asia-Pacific (Level I only)', tags=>['jun2019exam']},
-    {date=>'2019-06-16', summary=>'Religious alternate exam date (Americas and EMEA, all levels)', tags=>['jun2019exam']},
-    {date=>'2019-06-17', summary=>'Religious alternate exam date (Asia Pacific, all levels)', tags=>['jun2019exam']},
-    # TODO: result announcement
-
-    # dec2019exam
-    {date=>'2019-01-24', summary=>'Exam registration open', tags=>['dec2019exam']},
-    {date=>'2019-03-27', summary=>'Early registration fee deadline', tags=>['dec2019exam']},
-    {date=>'2019-08-14', summary=>'Standard registration fee deadline', tags=>['dec2019exam']},
-    {date=>'2019-09-11', summary=>'Final (late) registration fee deadline', tags=>['dec2019exam']},
-    {date=>'2019-12-07', summary=>'Exam day', tags=>['dec2019exam']},
-);
-
 1;
 # ABSTRACT: CFA exam calendar
 
@@ -51,3 +24,31 @@ This module provides CFA exam calendar using the L<Calendar::Dates> interface.
 L<https://www.cfainstitute.org/programs/cfa>
 
 L<https://en.wikipedia.org/wiki/Chartered_Financial_Analyst>
+
+=cut
+
+__DATA__
+# dec2018exam
+2019-01-23;Exam results announcement (Dec 2018, Levels I & II);dec2018exam
+
+# jun2019exam
+2018-10-15;First deadline to request disability accommodations;jun2019exam
+2018-10-17;Early registration fee deadline;jun2019exam
+2019-02-13;Standard registration fee deadline;jun2019exam
+2019-02-18;Second deadline to request disability accommodations;jun2019exam
+2019-03-13;Final (late) registration fee deadline;jun2019exam
+2019-03-18;Final deadline to request disability accommodations;jun2019exam
+# mid-may 2019, admission tickets available
+2019-06-11;Deadline for submission of test center change requests;jun2019exam
+2019-06-15;Exam day: Asia-Pacific (Levels II & III), Americas and EMEA (all levels);jun2019exam
+2019-06-16;Exam day: Asia-Pacific (Level I only);jun2019exam
+2019-06-16;Religious alternate exam date (Americas and EMEA, all levels);jun2019exam
+2019-06-17;Religious alternate exam date (Asia Pacific, all levels);jun2019exam
+# TODO: result announcement
+
+# dec2019exam
+2019-01-24;Exam registration open;dec2019exam
+2019-03-27;Early registration fee deadline;dec2019exam
+2019-08-14;Standard registration fee deadline;dec2019exam
+2019-09-11;Final (late) registration fee deadline;dec2019exam
+2019-12-07;Exam day;dec2019exam
